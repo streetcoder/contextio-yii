58d7c51bb6611060857236d215f9dc7889a45c48
Shimul <proshimul@yahoo.com>
2014-01-13 12:11:17 +0600

    introduce the extension for yii

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../yii/framework.1.1.14/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
